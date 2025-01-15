b1f42d12a344603d209db826fd6e664fb7fffb6e
Рамзин Денис <d.ramzin@dhabits.ru>
2025-01-15 11:43:44 +0300

    Previous chats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { getServerSession } from "next-auth";
 
 import Chat from "./components/Chat";
 import { Separator } from "@/components/ui/separator";
+import PreviousChats from "./components/PreviousChats";
+import { Suspense } from "react";
 
 export default async function Home() {
   const session = await getServerSession();
@@ -12,6 +14,10 @@ export default async function Home() {
       {!session?.user?.name && <div>You need to log in to use this chat.</div>}
       {session?.user?.name && (
         <>
+          <Suspense fallback={<p>Previous chats loading...</p>}>
+            <PreviousChats />
+          </Suspense>
+          <h4 className="mt-5 text-2xl font-bold">New Chat Session</h4>
           <Separator className="my-5" />
           <Chat />
         </>
